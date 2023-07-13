d1b038734f3b1666cdc0bb3e1ee61d35838520f0
Andre Pinto <andrefjpinto@gmail.com>
2023-07-13 15:45:44 +0100

    feat(api): add dbcontext

---
+++ investments-tracker/InvestmentsTrackerContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace investments_tracker;
+
+public partial class InvestmentsTrackerContext : DbContext
+{
+    public InvestmentsTrackerContext()
+    {
+    }
+
+    public InvestmentsTrackerContext(DbContextOptions<InvestmentsTrackerContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Name=ConnectionStrings:InvestmentsTracker");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
